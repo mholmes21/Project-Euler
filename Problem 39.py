8c5af7f36e854311c91dcc99b101e0ff467c701d
mholmes21 <80144438+mholmes21@users.noreply.github.com>
2021-03-06 14:18:19 -0800

    problem files from laptop

--- Problem 39.py
+++ Problem 39.py
@@ -1,20 +1,35 @@
-import math, time
-N = 1000
-scores = dict()
+# If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+#{20,48,52}, {24,45,51}, {30,40,50}
+# For which value of p ≤ 1000, is the number of solutions maximised?
 
-start = time.time()
-for p in range(6, N+1, 2):
-    count = 0
-    for a in range(1, p//2):
-        for b in range(1, a):
-            c = math.sqrt(a**2 + b**2)
-            if a+b+c == p:
-                count += 1
-    if count != 0:
-        scores.update({p: count})
-        #print(p, count)
+# Set maximum bound
+N = 100
+i = 1
+#print("Here's the first few pythagorean triples")
 
-print(max(scores, key=scores.get))
-end = time.time()
+p = 200
+perimeters = list()
 
-print(end-start, "seconds")
+for y in range(N):
+    for x in range(y+1, N):
+
+        new_x = (x+1)**2 - (y+1)**2
+        new_y = 2*(x+1)*(y+1)
+        new_z = (x+1)**2 + (y+1)**2
+        #print("i:", i, "(", new_x, ",", new_y, ",", new_z, ")")
+        i+=1
+
+        tmp = new_x + new_y + new_z
+
+        if new_x > p or new_y > p or new_z > p:
+            #print("Failed attempt with:", tmp)
+            continue
+        else:
+            if tmp == 120:
+                print("Success with;", new_x, new_y, new_z)
+            perimeters.append(tmp)
+
+
+perimeters.sort()
+print("")
+print(perimeters)
